b41068693536b374bd2030da01e51e9a15567c10
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-08-17 14:49:18 -0300

    try/catch - tratando erros

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,7 +10,16 @@ public class Program {
 		// Try/Catch - Treino - Dev Superior
 		
 		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
+		// Scanner sc = new Scanner(System.in);
+		
+		
+		method1();
+		
+		System.out.println("End of program");
+		
+		
+		
+		/*
 		try {
 			String[] vect = sc.nextLine().split(" "); // split = corta o elemento
 			int position = sc.nextInt();
@@ -22,7 +31,30 @@ public class Program {
 		}
 		
 		System.out.println("End of program");
+		*/
+		
+	}
+	public static void method1() {
+		System.out.println("***METHOD1 START***");
+		method2();
+		System.out.println("***METHOD1 END***");
+	}
+	
+	public static void method2() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("***METHOD2 START***");
+		try {
+			String[] vect = sc.nextLine().split(" "); // split = corta o elemento
+			int position = sc.nextInt();
+		System.out.println(vect[position]);
+		} catch (ArrayIndexOutOfBoundsException e){
+			System.out.println("Invalid position! ");
+			e.printStackTrace();
+			sc.next();
+		} catch (InputMismatchException e) {
+			System.out.println("Input erro");
+		}
+		System.out.println("***METHOD2 END***");
 		sc.close();
 	}
-
 }
